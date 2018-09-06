4e30208ee7e31ae4d72b01706cd50ec8cf089619
David Sjölander <DSA@qlik.com>
2018-09-06 11:19:41 +0200

    initial committ

---
+++ index.js
@@ -0,0 +1,9 @@
+//Fetch all new (not assigned, no project) Qlik Core issues from qlik-oss. Exclude possibiliites (picasso.js..)
+ 
+
+//Store issues or stream to engine
+
+//keep current state, if any changes reload
+
+//reload 
+
